51a55631484d11c6f70b6c3ecb305b5b9ab66335
David Kreitter <davidkreitter@gmail.com>
2017-06-17 21:25:43 +0000

    moved hello.rb to git folder

---
+++ hello.rb
@@ -0,0 +1,11 @@
+gift = [
+          "half of a leftover burrito",
+          "a day-old cup of coffee",
+          "a tesla",
+          "a life-size quadrocopter",
+          "a slice of stale wedding cake",
+          "a medium-sized island off the coast of Sumatra",
+          "a visual prosthesis",
+          "a nutsack",
+         ]
+  puts "All I got for valentine's day was #{gift.sample}."
